7713ca2988fa3f831e4b3de960f2aa4120a53584
Scott <smasherprog@gmail.com>
2017-03-18 10:37:22 -0700

    updating websockets library

--- uwebsockets/benchmarks/ws.js
+++
@@ -1,8 +0,0 @@
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({ port: 3000 });
-
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    ws.send(message, {binary: true});
-  });
-});

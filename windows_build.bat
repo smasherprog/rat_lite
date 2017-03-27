0e383879175dd97a0805407e8bb3567caf6785f4
Scott <smasherprog@gmail.com>
2017-03-26 19:26:49 -0700

    Update windows_build.bat

--- windows_build.bat
+++ windows_build.bat
@@ -8,12 +8,3 @@ vcpkg install fltk boost openssl libjpeg-turbo zlib
 cd ..
 CMake  .. "-DCMAKE_TOOLCHAIN_FILE=build\vcpkg\scripts\buildsystems\vcpkg.cmake"
 cd ..
-   if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
-      CMAKE_URL="https://cmake.org/files/v3.7/cmake-3.7.2-Linux-x86_64.tar.gz"
-      mkdir cmake && travis_retry wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
-      export PATH=${DEPS_DIR}/cmake/bin:${PATH}
-    else
-      brew upgrade cmake || brew install cmake
-    fi
-  - cmake --version
- 

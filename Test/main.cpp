942e1499ac70cbd97e729cc5dffac506597d2581
Scott <smasherprog@gmail.com>
2017-08-27 13:19:21 -0700

    Squashed 'libs/clipboard_lite/' changes from 5f143cb..2b06d8f
    2b06d8f Update README.md
    b213de4 Updated public APi to be more clear
    
    git-subtree-dir: libs/clipboard_lite
    git-subtree-split: 2b06d8f16539a7213b96c0051d93bbb0adaf69bc

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,8 +1,8 @@
 #include "Clipboard_Lite.h"
-#include <string>
-#include <iostream>
-#include <thread>
 #include <chrono>
+#include <iostream>
+#include <string>
+#include <thread>
 
 // THESE LIBRARIES ARE HERE FOR CONVINIENCE!! They are SLOW and ONLY USED FOR
 // HOW THE LIBRARY WORKS!
@@ -15,35 +15,36 @@
 
 using namespace std::chrono_literals;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 
     auto counter = 0;
     std::cout << "Clipboard Example running" << std::endl;
     auto clipboard = SL::Clipboard_Lite::CreateClipboard()
-        .onText([](const std::string& text) {
-        std::cout << "onText" << std::endl;
-        std::cout << text << std::endl;
-    }).onImage([&](const SL::Clipboard_Lite::Image& image) {
-        counter++;
-        auto s = std::to_string(counter) + std::string(" M");
+                         ->onText([](const std::string &text) {
+                             std::cout << "onText" << std::endl;
+                             std::cout << text << std::endl;
+                         })
+                         ->onImage([&](const SL::Clipboard_Lite::Image &image) {
+                             counter++;
+                             auto s = std::to_string(counter) + std::string(" M");
 
-        if (image.PixelStride == 4) {
-            s+= std::string(".png");
-            lodepng::encode(s.c_str(), image.Data.get(), image.Width, image.Height);
-        }
-        else if (image.PixelStride == 3) {
-            s+= std::string(".jpg");
-            tje_encode_to_file(s.c_str(), image.Width, image.Height, 3, image.Data.get());
-        }
-        std::cout << "onImage Height=" << image.Height << " Width=" << image.Width << std::endl;
-    })
-        .run();
+                             if (image.PixelStride == 4) {
+                                 s += std::string(".png");
+                                 lodepng::encode(s.c_str(), image.Data.get(), image.Width, image.Height);
+                             }
+                             else if (image.PixelStride == 3) {
+                                 s += std::string(".jpg");
+                                 tje_encode_to_file(s.c_str(), image.Width, image.Height, 3, image.Data.get());
+                             }
+                             std::cout << "onImage Height=" << image.Height << " Width=" << image.Width << std::endl;
+                         })
+                         ->run();
 
     std::this_thread::sleep_for(1s);
     std::cout << "Copying Text to clipboard" << std::endl;
     std::string txt = "pasted text";
-    clipboard.copy(txt);
-
+    clipboard->copy(txt);
 
     std::this_thread::sleep_for(2s);
     auto i = 0;
@@ -52,10 +53,9 @@ int main(int argc, char* argv[]) {
         std::cout << "Copying Text to clipboard a " << i++ << " time" << std::endl;
         txt = "pasted text a time ";
         txt += std::to_string(i);
-        clipboard.copy(txt);
+        clipboard->copy(txt);
         std::this_thread::sleep_for(10s);
     }
-
 
     return 0;
 }
--- include/Clipboard_Lite.h
+++ include/Clipboard_Lite.h
@@ -1,42 +1,35 @@
 #pragma once
-#include <memory>
 #include <functional>
+#include <memory>
 
 namespace SL {
-    namespace Clipboard_Lite {
-        //image data will be in r, g, b format   or r, g, b, a format
-        struct Image {
-            std::shared_ptr<unsigned char> Data;
-            int Height = 0;
-            int Width = 0;
-            int PixelStride = 0;
-        };
-        class Clipboard_ManagerImpl;
-        class Clipboard_Manager {
-            std::shared_ptr<Clipboard_ManagerImpl> Impl_;
-        public:
-            Clipboard_Manager(const std::shared_ptr<Clipboard_ManagerImpl>& impl) : Impl_(impl) {}
-            Clipboard_Manager() {}
+namespace Clipboard_Lite {
+    // image data will be in r, g, b format   or r, g, b, a format
+    struct Image {
+        std::shared_ptr<unsigned char> Data;
+        int Height = 0;
+        int Width = 0;
+        int PixelStride = 0;
+    };
+    class Clipboard_ManagerImpl;
+    class Clipboard_Manager {
+        std::shared_ptr<Clipboard_ManagerImpl> Impl_;
 
-            //copy text into the clipboard
-            void copy(const std::string& text);
-           
-            void copy(const Image& img);
+      public:
+        Clipboard_Manager(const std::shared_ptr<Clipboard_ManagerImpl> &impl) : Impl_(impl) {}
+        // copy text into the clipboard
+        void copy(const std::string &text);
+        // copy an image into the clipboard... image data will be in r, g, b format   or r, g, b, a format
+        void copy(const Image &img);
+    };
+    class IClipboard_Configuration {
+      public:
+        virtual ~IClipboard_Configuration() {}
+        virtual std::shared_ptr<IClipboard_Configuration> onText(const std::function<void(const std::string &text)> &handle) = 0;
+        virtual std::shared_ptr<IClipboard_Configuration> onImage(const std::function<void(const Image &image)> &handle) = 0;
+        virtual std::shared_ptr<Clipboard_Manager> run() = 0;
+    };
 
-            operator bool() const { return Impl_.operator bool(); }
-            //will stop the library from processing frames and release all memory
-            void destroy() { Impl_.reset(); }
-        };
-        class Clipboard_Configuration {
-            std::shared_ptr<Clipboard_ManagerImpl> Impl_;
-        public:
-            Clipboard_Configuration(const std::shared_ptr<Clipboard_ManagerImpl>& impl) : Impl_(impl) {}
-
-            Clipboard_Configuration onText(const std::function<void(const std::string& text)>& handle);
-            Clipboard_Configuration onImage(const std::function<void(const Image& image)>& handle);
-
-            Clipboard_Manager run();
-        };
-        Clipboard_Configuration CreateClipboard();
-    }
-}
+    std::shared_ptr<IClipboard_Configuration> CreateClipboard();
+} // namespace Clipboard_Lite
+} // namespace SL

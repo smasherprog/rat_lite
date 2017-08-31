61594bca9f3271170034dc454e9bb0da9bd618ee
scott <smasherprog@gmail.com>
2017-08-30 21:04:53 -0700

    adding linux client

--- src/client/linux/ClientWindow.cpp
+++ src/client/linux/ClientWindow.cpp
@@ -1,32 +1,41 @@
 #include "ClientWindow.h"
 #include "Configs.h"
+#include <gtk/gtk.h>
 
-namespace SL {
-    namespace RAT_Client {
-        class ClientWindowImpl {
-            std::shared_ptr<SL::RAT::Client_Config> Config;
-        public:
 
-            ClientWindowImpl(std::shared_ptr<SL::RAT::Client_Config>& config, const std::string& host) :Config(config) {
-              
+namespace SL
+{
+namespace RAT_Client
+{
+    class ClientWindowImpl
+    {
+        std::shared_ptr<SL::RAT::Client_Config> Config;
+
+    public:
+        ClientWindowImpl(std::shared_ptr<SL::RAT::Client_Config>& config, const std::string& host)
+            : Config(config)
+        {
+        }
+        void Run()
+        {
+            while(1) {
+               
             }
-            void Run() {
-
-            }
-
-
-        };
-
-
-        ClientWindow::ClientWindow(std::shared_ptr<SL::RAT::Client_Config>& config, const std::string& host) {
-            ClientWindowImpl_ = new ClientWindowImpl(config, host);
         }
-        ClientWindow::~ClientWindow() {
-            delete ClientWindowImpl_;
-        }
+    };
 
-        void ClientWindow::Run() {
-            ClientWindowImpl_->Run();
-        }
+    ClientWindow::ClientWindow(std::shared_ptr<SL::RAT::Client_Config>& config, const std::string& host)
+    {
+        ClientWindowImpl_ = new ClientWindowImpl(config, host);
     }
+    ClientWindow::~ClientWindow()
+    {
+        delete ClientWindowImpl_;
+    }
+
+    void ClientWindow::Run()
+    {
+        ClientWindowImpl_->Run();
+    }
+}
 }

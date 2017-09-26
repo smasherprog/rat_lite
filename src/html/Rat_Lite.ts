28f42fea0a166e34349c49909de6a67318c7fa17
Scott <smasherprog@gmail.com>
2017-09-25 21:31:07 -0700

    working on html implementation of client

--- src/html/Rat_Lite.ts
+++ src/html/Rat_Lite.ts
@@ -1,94 +1,247 @@
 ﻿
-import *from './Input_Lite';
-module SL
-{
-    export module RAT_Lite
-    {
-        export class Point {
-            static sizeof() : number { return 8; } // actual bytes used
-            constructor(public X : number, public Y : number) {}
-            static FromArray(data : Uint8Array) : Point
-            {
-                var arr = new Int32Array(data.slice(0, this.sizeof()).buffer);
-                return new Point(arr[0], arr[1]);
-            }
-            Fill(d : ArrayBuffer, offset : number) : void
-            {
-                var dt = new DataView(d, offset);
-                dt.setInt32(0, this.X, true);
-                dt.setInt32(4, this.Y, true);
-            }
-        };
-        export class Rect {
-            static sizeof() : number { return 8 + Point.sizeof(); } // actual bytes used
-            constructor(public Origin : Point, public Height : number, public Width : number) {}
-            static FromArray(data : Uint8Array) : Rect
-            {
-                var arr = new Int32Array(data.slice(0, this.sizeof()).buffer);
-                return new Rect(new Point(arr[0], arr[1]), arr[2], arr[3]);
-            }
-            Fill(d : ArrayBuffer, offset : number) : void
-            {
-                this.Origin.Fill(d, offset);
-                var dt = new DataView(d, offset + Point.sizeof());
-                dt.setInt32(0, this.Height, true);
-                dt.setInt32(4, this.Width, true);
-            }
-        };
-        enum PACKET_TYPES {
-            INVALID,
-            HTTP_MSG,
-            ONMONITORSCHANGED,
-            ONFRAMECHANGED,
-            ONNEWFRAME,
-            ONMOUSEIMAGECHANGED,
-            ONMOUSEPOSITIONCHANGED,
-            ONKEYUP,
-            ONKEYDOWN,
-            ONMOUSEUP,
-            ONMOUSEDOWN,
-            ONMOUSESCROLL,
-            ONCLIPBOARDTEXTCHANGED,
-            // use LAST_PACKET_TYPE as the starting point of your custom packet types. Everything before this is used internally by the library
-            LAST_PACKET_TYPE
-        };
+import { KeyCodes, MouseButtons, KeyEvent } from './Input_Lite';
 
-        export class IClientDriver {
 
-          public
-            ShareClipboard(share : boolean) : void {}
+export enum OpCode { CONTINUATION = 0, TEXT = 1, BINARY = 2, CLOSE = 8, PING = 9, PONG = 10, INVALID = 255 };
+export class WSMessage {
+    data: DataView;
+    code: OpCode;
+};
+export class Point {
+    X: number;
+    Y: number;
+};
+export class Rect {
+    Origin: Point;
+    Height: number;
+    Width: number;
+};
+export class Image {
+    Rect: Rect;
+    Data = [];
+    Length: number;
+};
+export class Monitor {
+    Id: number;
+    Index: number;
+    Height: number;
+    Width: number;
+    // Offsets are the number of pixels that a monitor can be from the origin. For example, users can shuffle their
+    // monitors around so this affects their offset.
+    OffsetX: number;
+    OffsetY: number;
+    Name: string;
+};
+enum PACKET_TYPES {
+    INVALID,
+    HTTP_MSG,
+    ONMONITORSCHANGED,
+    ONFRAMECHANGED,
+    ONNEWFRAME,
+    ONMOUSEIMAGECHANGED,
+    ONMOUSEPOSITIONCHANGED,
+    ONKEYUP,
+    ONKEYDOWN,
+    ONMOUSEUP,
+    ONMOUSEDOWN,
+    ONMOUSESCROLL,
+    ONCLIPBOARDTEXTCHANGED,
+    // use LAST_PACKET_TYPE as the starting point of your custom packet types. Everything before this is used internally by the library
+    LAST_PACKET_TYPE
+};
 
-          public
-            ShareClipboard() : boolean {}
-          public
-            SendKeyUp(SL::Input_Lite::KeyCodes key) : void {}
-          public
-            SendKeyDown(SL::Input_Lite::KeyCodes key) : void {}
-          public
-            SendMouseUp(const Input_Lite::MouseButtons button) : void {}
-          public
-            SendMouseDown(const Input_Lite::MouseButtons button) : void {}
-          public
-            SendMouseScroll(int offset) : void {}
-          public
-            SendMousePosition(const Point &pos) : void {}
-          public
-            SendClipboardChanged(const std::string &text) : void {}
-        };
+export class IClientDriver {
+    private ShareClipboard = false;
+    protected Monitors: Array<Monitor>;
+    protected WebSocket_: WebSocket;
 
-        class RAT_LITE_EXTERN IClientDriverConfiguration : public INetworkHandlers<IClientDriverConfiguration> {
-          public:
-            virtual ~IClientDriverConfiguration() {}
-            virtual std::shared_ptr<IClientDriverConfiguration>
-            onMonitorsChanged(const std::function<void(const std::vector<Screen_Capture::Monitor> &)> &callback) = 0;
-            virtual std::shared_ptr<IClientDriverConfiguration>
-            onFrameChanged(const std::function<void(const Image &, const SL::Screen_Capture::Monitor &)> &callback) = 0;
-            virtual std::shared_ptr<IClientDriverConfiguration>
-            onNewFrame(const std::function<void(const Image &, const SL::Screen_Capture::Monitor &)> &callback) = 0;
-            virtual std::shared_ptr<IClientDriverConfiguration> onMouseImageChanged(const std::function<void(const Image &)> &callback) = 0;
-            virtual std::shared_ptr<IClientDriverConfiguration> onMousePositionChanged(const std::function<void(const Point &)> &callback) = 0;
-            virtual std::shared_ptr<IClientDriverConfiguration> onClipboardChanged(const std::function<void(const std::string &)> &callback) = 0;
-            virtual std::shared_ptr<IClientDriver> Build(const std::shared_ptr<SL::WS_LITE::IWSClient_Configuration> &wsclientconfig) = 0;
-        };
+    protected onConnection_: (ws: WebSocket) => void;
+    protected onMessage_: (ws: WebSocket, message: WSMessage) => void;
+    protected onDisconnection_: (ws: WebSocket, code: number, message: string) => void;
+    protected onMonitorsChanged_: (monitors: Monitor[]) => void;
+    protected onFrameChanged_: (image: Image, monitor: Monitor) => void;
+    protected onNewFrame_: (image: Image, monitor: Monitor) => void;
+    protected onMouseImageChanged_: (image: Image) => void;
+    protected onMousePositionChanged_: (point: Point) => void;
+    protected onClipboardChanged_: (clipstring: string) => void;
+
+
+    setShareClipboard(share: boolean): void { this.ShareClipboard = share; }
+    getShareClipboard(): boolean { return this.ShareClipboard; }
+    SendKeyUp(key: KeyEvent): void { }
+    SendKeyDown(key: KeyCodes): void { }
+    SendMouseUp(button: MouseButtons): void { }
+    SendMouseDown(button: MouseButtons): void { }
+    SendMouseScroll(offset: number): void { }
+    SendMousePosition(pos: Point): void { }
+    SendClipboardChanged(text: string): void { }
+
+};
+
+export class IClientDriverConfiguration extends IClientDriver {
+    onConnection(callback: (ws: WebSocket) => void): IClientDriverConfiguration {
+        this.onConnection_ = callback;
+        return this;
     }
+    onMessage(callback: (ws: WebSocket, message: WSMessage) => void): IClientDriverConfiguration {
+        this.onMessage_ = callback;
+        return this;
+    }
+    onDisconnection(callback: (ws: WebSocket, code: number, message: string) => void): IClientDriverConfiguration {
+        this.onDisconnection_ = callback;
+        return this;
+    }
+    onMonitorsChanged(callback: (monitors: Monitor[]) => void): IClientDriverConfiguration {
+        this.onMonitorsChanged_ = callback;
+        return this;
+    }
+    onFrameChanged(callback: (image: Image, monitor: Monitor) => void): IClientDriverConfiguration {
+        this.onFrameChanged_ = callback;
+        return this;
+    }
+    onNewFrame(callback: (image: Image, monitor: Monitor) => void): IClientDriverConfiguration {
+        this.onNewFrame_ = callback;
+        return this;
+    }
+    onMouseImageChanged(callback: (image: Image) => void): IClientDriverConfiguration {
+        this.onMouseImageChanged_ = callback;
+        return this;
+    }
+    onMousePositionChanged(callback: (point: Point) => void): IClientDriverConfiguration {
+        this.onMousePositionChanged_ = callback;
+        return this;
+    }
+    onClipboardChanged(callback: (clipstring: string) => void): IClientDriverConfiguration {
+        this.onClipboardChanged_ = callback;
+        return this;
+    }
+
+    private MonitorsChanged(ws: WebSocket, dataview: DataView) {
+        if (!this.onMonitorsChanged_)
+            return;
+        let sizeofmonitor = 6 * 4 + 128;
+        let num = dataview.byteLength / sizeofmonitor;
+        this.Monitors = new Array<Monitor>();
+        if (dataview.byteLength == num * sizeofmonitor && num < 8) {
+            for (var i = 0; i < num; i++) {
+                var name = '';
+                for (var i = 0, strLen = 128; i < strLen; i++) {
+                    name += String.fromCharCode.apply(dataview.getUint8(20 +i));
+                }
+                this.Monitors.push({
+                    Id: dataview.getInt32(0),
+                    Index: dataview.getInt32(4),
+                    Height: dataview.getInt32(8),
+                    Width: dataview.getInt32(12),
+                    OffsetX: dataview.getInt32(16),
+                    OffsetY: dataview.getInt32(20),
+                    Name: name
+                });
+            }
+            return this.onMonitorsChanged_(this.Monitors);
+        }
+        else if (dataview.byteLength  == 0) {
+            // it is possible to have no monitors.. shouldnt disconnect in that case
+            return this.onMonitorsChanged_(this.Monitors);
+        }
+        if (this.onDisconnection_) {
+            this.onDisconnection_(ws, 1000, "Invalid Monitor Count");
+        }
+        ws.close(1000, "Invalid Monitor Count");
+    }
+    private Frame(ws: WebSocket, dataview: DataView) {
+
+    }
+    private MouseImageChanged(ws: WebSocket, dataview: DataView) {
+
+    }
+    private MousePositionChanged(ws: WebSocket, dataview: DataView) {
+        if (!this.onMousePositionChanged_)
+            return;
+        if (dataview.byteLength == 8) {
+            var p ={
+                X: dataview.getInt32(0),
+                Y: dataview.getInt32(4)
+            };
+            return this.onMousePositionChanged_(p);
+        }
+        if (this.onDisconnection_) {
+            this.onDisconnection_(ws, 1000, "Received invalid lenght on onMousePositionChanged");
+        }
+        ws.close(1000, "Received invalid lenght on onMousePositionChanged"); 
+    }
+    private ClipboardTextChanged(dataview: DataView) {
+
+    }
+    Build(ws: WebSocket): IClientDriver {
+        var self = this;
+        ws.binaryType = 'arraybuffer';
+        ws.onopen = () => {
+            console.log('onopen');
+            if (self.onConnection_) {
+                self.onConnection_(ws);
+            }
+        };
+        ws.onclose = (ev: CloseEvent) => {
+            console.log('onclose');
+            if (self.onDisconnection_) {
+                self.onDisconnection_(ws, ev.code, ev.reason);
+            }
+        };
+        ws.onmessage = (ev: MessageEvent) => {
+            console.log('onmessage');
+            var t0 = performance.now();
+            var data = new DataView(ev.data);
+            var packettype = <PACKET_TYPES>data.getInt32(0, true);
+
+            switch (packettype) {
+                case PACKET_TYPES.ONMONITORSCHANGED:
+                    this.MonitorsChanged(ws, new DataView(ev.data, 4));
+                    break;
+                case PACKET_TYPES.ONFRAMECHANGED:
+                    if (this.onFrameChanged_) {
+                        this.Frame(ws, new DataView(ev.data, 4));
+                    }
+                    break;
+                case PACKET_TYPES.ONNEWFRAME:
+                    if (this.onNewFrame_) {
+                        this.Frame(ws, new DataView(ev.data, 4));
+                    }
+                    break;
+                case PACKET_TYPES.ONMOUSEIMAGECHANGED:
+                    this.MouseImageChanged(ws, new DataView(ev.data, 4));
+                    break;
+                case PACKET_TYPES.ONMOUSEPOSITIONCHANGED:
+                    this.MousePositionChanged(ws, new DataView(ev.data, 4));
+                    break;
+                case PACKET_TYPES.ONCLIPBOARDTEXTCHANGED:
+                    this.ClipboardTextChanged(new DataView(ev.data, 4));
+                    break;
+                default:
+                    if (this.onMessage_) {
+                        var r = new WSMessage();
+                        r.data = new DataView(ev.data, 4);
+                        if (ev.data instanceof ArrayBuffer) {
+                            r.code = OpCode.BINARY;
+                        } else if (typeof ev.data === "string") {
+                            r.code = OpCode.TEXT;
+                        }
+                        this.onMessage_(ws, r); // pass up the chain
+                    }
+                    break;
+            }
+            var t1 = performance.now();
+            // comment this line out to see performance issues... My machine takes 0 to 6 ms to complete each receive
+            console.log("took " + (t1 - t0) + " milliseconds to process the receive loop");
+
+
+
+
+
+        };
+        return this;
+    }
+};
+export function CreateClientDriverConfiguration(): IClientDriverConfiguration {
+    return new IClientDriverConfiguration();
 }

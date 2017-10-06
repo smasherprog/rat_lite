49f36b3c0c3168fcfb60633ccadca0eac39a9f6e
Scott <smasherprog@gmail.com>
2017-10-05 19:05:10 -0700

    update to cmake

---
+++ modules/FindLibJpegTurbo.cmake
@@ -0,0 +1,22 @@
+include (FindPackageHandleStandardArgs)
+
+find_path(LIBJPEGTURBO_INCLUDE_DIRS turbojpeg.h
+	PATHS /usr/local/opt/jpeg-turbo/include
+)
+set(JPEG_NAMES jpeg turbojpeg)
+
+FOREACH(LIB ${JPEG_NAMES})
+        FIND_LIBRARY(FOUND_LIB_${LIB} ${LIB}
+		   PATHS /usr/local/opt/jpeg-turbo/lib
+                   /opt/libjpeg-turbo/lib64
+                   /opt/libjpeg-turbo/lib
+		)
+		if(FOUND_LIB_${LIB})
+			LIST(APPEND LIBJPEGTURBO_LIBRARIES ${FOUND_LIB_${LIB}})
+		endif()
+
+ENDFOREACH(LIB)
+   
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LIBJPEGTURBO DEFAULT_MSG LIBJPEGTURBO_LIBRARIES LIBJPEGTURBO_INCLUDE_DIRS)
+
+mark_as_advanced(LIBJPEGTURBO_LIBRARIES LIBJPEGTURBO_INCLUDE_DIRS )
